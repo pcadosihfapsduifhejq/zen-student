58f4e7149b1a7135e19533f33560a49afc6595eb
rahuljaijiv <rahuljaijiv@gmail.com>
2014-06-15 23:02:28 -0700

    initial commit

---
+++ src/zenlab/Drawing.java
@@ -0,0 +1,10 @@
+package zenlab;
+
+import zen.core.Zen;
+
+public class Drawing {
+	public static void main(String[] args) {
+		Zen.create(500, 500);
+		Zen.drawText("Welcome to Java!", 160, 250);
+	}
+}
